5cabc8e49da259066a6b42fd4411a67f1a079815
Cookies <souyuuki12@gmail.com>
2022-06-18 22:23:40 +0800

    Updated root file
    To centralize imports/exports to prevent cyclic dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,5 @@
-import React, {
-  useState,
-  createContext,
-  useContext
-} from 'react'
+import ListenerProvider from "./provider/Provider";
+import TouchableOpacityListener from "./components/TouchableOpacity";
 
-const ListenerContext = createContext({})
-
-export const useListenerProvider = () => {
-  const { addEventListener, emit }: any = useContext(ListenerContext)
-
-  return { addEventListener, emit }
-}
-
-const ListenerProvider = (props: any) => {
-  const [listeners, setListeners]: any = useState({})
-
-  const addEventListener = (event: string, callback: Function) => {
-    setListeners({
-      ...listeners,
-      [event]: callback
-    })
-  }
-
-  const emit = (event: string, data: any) => {
-    if (listeners[event]) {
-      listeners[event](data)
-    }
-  }
-
-  const value = { addEventListener, emit }
-
-  return (
-    <ListenerContext.Provider
-      value={value}
-    >
-      {props.children}
-    </ListenerContext.Provider>
-  )
-}
-
-export default ListenerProvider;
+export default ListenerProvider
+export { TouchableOpacityListener }
